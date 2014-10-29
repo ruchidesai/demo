e6a30f6dafe5f1fe5877c63fc5dd5c1469576c76
Ruchi Vivek Desai <ruchi.desai@ufl.edu>
2014-10-29 15:55:03 -0400

    Add files for final prototype

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Steps to run:
  1. Clone repo
  2. cd to app folder
  3. `. ../env/bin/activate`
- 4. `python routes.py`
- 5. Copy and paste the ip displayed on console in your browser to load the web interface.
+ 4. pip install Flask
+ 5. `python routes.py`
+ 6. Copy and paste the ip displayed on console in your browser to load the web interface.
--- sampleReportWebsite/app/routes.py
+++ sampleReportWebsite/app/routes.py
@@ -6,25 +6,33 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
-@app.route('/alerts')
-def alerts():
-	return render_template('alerts.html')
+@app.route('/report_home')
+def report_home():
+	return render_template('report_home.html')
+
+@app.route('/graph1')
+def graph1():
+	return render_template('graph1.html')
+
+@app.route('/reports')
+def reports():
+	return render_template('reports.html')
+
+@app.route('/report1')
+def report1():
+	return render_template('report1.html')
 
 @app.route('/details')
 def details():
 	return render_template('details.html')
 
-@app.route('/errors')
-def errors():
-	return render_template('errors.html')
+@app.route('/redcap_site')
+def redcap_site():
+	return render_template('redcap_site.html')
 
-@app.route('/summary')
-def summary():
-	return render_template('summary.html')
-
-@app.route('/graph1')
-def graph1():
-	return render_template('graph1.html')
+@app.route('/graph2')
+def graph2():
+	return render_template('graph2.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
